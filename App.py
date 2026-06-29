02e44afdaafc9fcd65a5245e266abfd9f71c5978
mert-kekik <mertkekik96@gmail.com>
2026-06-29 17:34:26 +0300

    Add equation solver using Streamlit and OCR

---
+++ App.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from PIL import Image
+from pix2tex.cli import LatexOCR
+from latex2sympy2 import latex2sympy
+import sympy
+st.title("Denklem çözücü")
+AI=LatexOCR()
+photo=st.camera_input("denklemin fotoğrafını çekin")
+if photo is not None :
+	st.success("fotoğraf alındı")
+	st.image(photo)
+	edited_photo=Image.open(photo)
+	denklem_metni=AI(edited_photo)
+	st.latex(denklem_metni)
+	try:
+		mat_ifade=latex2sympy(denklem_metni)
+		cozum=sympy.solve(mat_ifade)
+		st.success(cozum)
+	except Exception as e :
+		st.error("denklem hatalı lütfen tekrar yükleyin")
+	
+	
